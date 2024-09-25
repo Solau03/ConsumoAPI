cd69f90dd70eba1f91ca7c7728ea1365822ea76e
Laura Quina <150872507+Solau03@users.noreply.github.com>
2024-09-25 18:34:03 -0500

    0.1

---
+++ script.js
@@ -0,0 +1,90 @@
+fetch("https://fake-api-vq1l.onrender.com/posts", {
+  method: "GET",
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIxLCJlbWFpbCI6ImxhdXJhLnF1aW5hQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY3MjEyNTAsImV4cCI6MTc0NDAwMTI1MH0.7oVmPZW4gE09vhQKVvWySb_TOW3vr6NHghs-ytjLzJs"
+  }
+})
+//pasarlo a json
+.then(response => response.json())
+//procesar la info
+.then(data => {
+
+  const list = document.getElementById("productos");
+
+
+  data.forEach( product => {
+    const il = document.createElement("li");
+    const images = JSON.parse(product.images)
+    // const img = document.createElement("img");
+    // img.src = images[0];
+    const myhtml = ` 
+      <div class="card">
+        <div class="card_landing" style"--i:${images[0]}>
+          <h5>${product.title} </h5>
+        </div>
+        <div class="card_info" > </div>
+        <img src="${images[0]}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${product.title}</h5>
+          <p class="card-text">${product.description}</p>
+          <a class="btn btn-warning">Edit</a>
+          <a onclick="deletePost(${product.id})" class="btn btn-danger">Delete</a>
+        </div>
+      </div>
+    `; 
+    il.innerHTML =  myhtml;
+    list.appendChild(il); 
+  })
+});
+
+
+function sendForm(){
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const value = document.getElementById("value");
+const image = document.getElementById("image");
+const body ={
+  title: title.value,
+  description: description.value,
+  value: value.value,
+  images: [image.value] 
+}
+
+
+fetch("https://fake-api-vq1l.onrender.com/posts", {
+  method: "POST", 
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIxLCJlbWFpbCI6ImxhdXJhLnF1aW5hQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY3MjEyNTAsImV4cCI6MTc0NDAwMTI1MH0.7oVmPZW4gE09vhQKVvWySb_TOW3vr6NHghs-ytjLzJs",
+    "Content-type": "application/json"
+  },
+  body: JSON.stringify(body)
+})
+.then( res => res.json())
+.then( res => {
+  console.log(
+    "respuesta de la api", res
+  )
+  title.value = "";
+  description.value = "";
+  value.value = "";
+  image.value = "";
+  location.reload();
+})
+
+}
+
+function deletePost(id){
+fetch(`https://fake-api-vq1l.onrender.com/posts/${id}`, {
+  method: "DELETE", 
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIxLCJlbWFpbCI6ImxhdXJhLnF1aW5hQHV0cC5lZHUuY28iLCJpYXQiOjE3MjY3MjEyNTAsImV4cCI6MTc0NDAwMTI1MH0.7oVmPZW4gE09vhQKVvWySb_TOW3vr6NHghs-ytjLzJs",
+  },
+})
+.then( res => res.json())
+.then( res => {
+  console.log(
+    "respuesta de la api", res
+  )
+  location.reload();
+})
+}
